24d2891c884fef0699a061918485d46068632eda
mussietarekegn <mussie.tarekegn@aastustudent.edu.et>
2025-12-09 22:26:40 +0300

    update and edit blog post

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView,CreateView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm,EditForm
 # Create your views here.
 
 # def home(request):
@@ -21,4 +21,11 @@ class AddPostView(CreateView):
     template_name='add_post.html'
     # fields='__all__'
     # fields=['title','body']
+
+class UpdatePostView(UpdateView):
+    model=Post
+    template_name='update_post.html'
+    # fields=['title','title_tag','body']
+    form_class=EditForm
+
     
